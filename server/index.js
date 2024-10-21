30bf82741a337a220a5730eec68e69cb5e37e2f0
pavankumarvn1518 <pavnkumarvn1518>
2024-10-21 19:08:02 +0530

    chore: Add middleware to parse JSON request bodies
    - Added `express.json()` middleware to handle JSON payloads in incoming requests.
    - Enables parsing of `req.body` in API routes for better request handling.

--- server/index.js
+++ server/index.js
@@ -3,6 +3,10 @@ import dotenv from 'dotenv';
 import { connectDb } from './database/db.js';  // Note the './'
 dotenv.config();
 const app = express();
+
+//using middlewares
+app.use(express.json());
+
 const port= process.env.PORT;
 
 
