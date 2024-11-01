c0a693e1c47025217cf9e090f19014555914b9d4
pavankumarvn1518 <pavnkumarvn1518>
2024-11-01 12:14:38 +0530

    user register and verify completed

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -2,51 +2,74 @@ import{ User} from "../models/user.js";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import sendMail from "../middleware/sendMail.js";
+import TryCatch from "../middleware/TryCatch.js";
 
-export const  register = async(req,res) =>{
-    try{
-        const { email,name,password }= req.body
-        let user =await User.findOne({email});
-        if(user)
-            return res.status(400).json({
-         message:"User already esxists"
-            });
-
-            const hashPassword = await bcrypt.hash(password,10);
-
-         user  = {
-                name,
-                email,
-                password:hashPassword
-            }
-    const otp =Math.floor(Math.random() *1000000);
-    const activationToken = jwt.sign({
+export const register = TryCatch(async (req, res) => {
+    const { email, name, password } = req.body;
+  
+    let user = await User.findOne({ email });
+  
+    if (user)
+      return res.status(400).json({
+        message: "User Already exists",
+      });
+  
+    const hashPassword = await bcrypt.hash(password, 10);
+  
+    user = {
+      name,
+      email,
+      password: hashPassword,
+    };
+  
+    const otp = Math.floor(Math.random() * 1000000);
+  
+    const activationToken = jwt.sign(
+      {
         user,
         otp,
-
-    },process.env.Activation_Secret,{
-        expiresIn:"5m",
-    }
-);
- const data = {
-    name,
-    otp,
- };
- await sendMail(
-    email,
-    "E lerning",
-    data
-
- )
- res.status(200).json({
-    message:"Otp sent your mail",
-    activationToken,
-
- });
-       // res.send("Register  Api") 
-    }catch(error){
-        res.status(500).json({
-            message:error.message,
-        });
-    }
-}
+      },
+      process.env.Activation_Secret,
+      {
+        expiresIn: "5m",
+      }
+    );
+  
+    const data = {
+      name,
+      otp,
+    };
+  
+    await sendMail(email, "E learning", data);
+  
+    res.status(200).json({
+      message: "Otp send to your mail",
+      activationToken,
+    });
+  });
+  
+  export const verifyUser = TryCatch(async (req, res) => {
+    const { otp, activationToken } = req.body;
+  
+    const verify = jwt.verify(activationToken, process.env.Activation_Secret);
+  
+    if (!verify)
+      return res.status(400).json({
+        message: "Otp Expired",
+      });
+  
+    if (verify.otp !== otp)
+      return res.status(400).json({
+        message: "Wrong Otp",
+      });
+  
+    await User.create({
+      name: verify.user.name,
+      email: verify.user.email,
+      password: verify.user.password,
+    });
+  
+    res.json({
+      message: "User Registered",
+    });
+  });
--- server/models/user.js
+++ server/models/user.js
@@ -1,29 +1,79 @@
+// import mongoose from "mongoose";
+
+// const schema = new mongoose.Schema(
+//   {
+//     name: {
+//       type: String,
+//       required: true,
+//     },
+//     email: {
+//       type: String,
+//       required: true,
+//       unique: true,
+//     },
+//     password: {
+//       type: String,
+//       required: true,
+//     },
+//     role: {
+//       type: String,
+//       default: "user",
+//     },
+//     mainrole: {
+//       type: String,
+//       default: "user",
+//     },
+//     subscription: [
+//       {
+//         type: mongoose.Schema.Types.ObjectId,
+//         ref: "Courses",
+//       },
+//     ],
+//     resetPasswordExpire: Date,
+//   },
+//   {
+//     timestamps: true,
+//   }
+// );
+
+// export const User = mongoose.model("User", schema);
+
 import mongoose from "mongoose";
-const schema = new mongoose.Schema({
-    name:{
-        type:String,
-        required:true,
-    },
-    email:{
-        type:String,
-        required:true,
-        unique:true,
-    },
-    name:{
-        type:String,
-        required:true,
-    },
-    role:{
-        type:String,
-        default:"user",
 
-    },subscription:[{
-        type:mongoose.Schema.Types.ObjectId,
-        ref:"Course",
-
+const schema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        password: {
+            type: String,
+            required: true,
+        },
+        role: {
+            type: String,
+            default: "user",
+        },
+        mainrole: {
+            type: String,
+            default: "user",
+        },
+        subscription: [
+            {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "Courses",
+            },
+        ],
+        resetPasswordExpire: Date,
     },
-    ],
-},{
-   timestamp:true,
-});
-export const User = mongoose.model("User",schema);
+    {
+        timestamps: true,
+    }
+);
+
+export const User = mongoose.model("User", schema);
